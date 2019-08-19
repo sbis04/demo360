8cfcf6a624fab28865a48bd1e3028aef3328467d
Souvik Biswas <sbis1999@gmail.com>
2019-08-19 19:00:21 +0530

    New image

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ export default class demo360 extends React.Component {
       <View style={styles.panel}>
         <View style={styles.greetingBox}>
           <Text style={styles.greeting}>
-            Welcome to Latvia
+            Welcome to React360
           </Text>
         </View>
       </View>
